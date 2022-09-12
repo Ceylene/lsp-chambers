71b70c4e5b776943ab75193cdc6f17d039a3e98c
Ceylene <ceylene.chambers@bison.howard.edu>
2022-09-12 19:57:22 -0400

    Commit for Assignment1

---
+++ lsp-chambers/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.howard.edu.lsp.assignment1;
+
+
+
+/**
+ * Ceylene Chambers
+ * 
+ * @author ceyle
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
